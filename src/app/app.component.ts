229354b2fed52b30e4ab7cd845512bcc8949fe87
alencartha <alencar_tha@hotmail.com>
2022-09-13 11:39:05 -0300

    setup estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+  template: `<h1>teste</h1><router-outlet></router-outlet>`
 })
 export class AppComponent {
 }
