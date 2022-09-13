6c4c711020166353df954676865c5ef6afc8ea64
alencartha <alencar_tha@hotmail.com>
2022-09-13 12:27:29 -0300

    coponentes compartilhados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//Components
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { DetailsComponent } from './details/details.component';
+
+//Módulos
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,7 +18,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    SharedModule
   ]  
 })
 export class PagesModule { }
