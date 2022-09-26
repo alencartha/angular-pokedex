4c9bee974600a2871bd0c6f5319079fd65114442
alencartha <alencar_tha@hotmail.com>
2022-09-26 11:31:46 -0300

    feature: filtra pokemons map, por meio da url

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -11,7 +11,7 @@ export class PokeListComponent implements OnInit {
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe(res => res)
+    this.pokeApiService.apiListAllPokemons.subscribe(res => console.log(res))
   }
 
 }
